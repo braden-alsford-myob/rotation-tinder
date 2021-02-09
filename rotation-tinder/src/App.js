5804dd3356468934edebcfc6fbab4a1a75fab9af
Leah Filkin <leah.filkin@myob.com>
2021-02-10 10:51:53 +1300

    continued making global component

--- rotation-tinder/src/App.js
+++ rotation-tinder/src/App.js
@@ -15,14 +15,54 @@ const App = () => {
         ]
     );
 
+    const [shortList, setShortList] = useState(
+        []
+    );
+
+    const [currentlySwiping, setCurrentlySwiping] = useState(
+        false
+    )
+
+    const swipeButtonClicked = () => {
+        setCurrentlySwiping(true)
+    }
+
+    const listButtonClicked = () => {
+        setCurrentlySwiping(false)
+    }
+
+    const bottomNavigation = (
+        <div>
+            <button onClick={swipeButtonClicked}> Swiping </button>
+            <button onClick={listButtonClicked}> List </button>
+        </div>
+    )
+
+    if (currentlySwiping) {
+        return (
+            <div>
+                <div>
+                    <button className="button">Add team</button>
+                    <SwipeyBoi allTeams={allTeams}/>
+                </div>
+                {bottomNavigation}
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <div>
+                <div>
+                    <button className="button">Add team</button>
+                    <div> hosiahpisahsc </div>
+                </div>
+                {bottomNavigation}
+            </div>
+        );
+    }
 
 
-  return (
-      <div>
-          <button className="button">Add team</button>
-          <SwipeyBoi allTeams={allTeams}/>
-      </div>
-  );
 };
 
 export default App;
