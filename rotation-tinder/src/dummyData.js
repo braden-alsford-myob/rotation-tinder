753b1c5c4d474c7d7a3b1fae93af83a97f003b46
James-Kehoe-MYOB <james.kehoe@myob.com>
2021-02-09 15:33:46 +1100

    adds dummy data

---
+++ rotation-tinder/src/dummyData.js
@@ -0,0 +1,75 @@
+export const data = [
+    {
+        id: 1,
+        image:
+            "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=PastelGreen&graphicType=Diamond&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale",
+        teamName: "Unicorn",
+        stack: ["React", "C#"],
+        description: "Our team consists of 2 senior devs, a BA and a QA",
+        product: "New Essentials",
+        location: "Melbourne",
+        domain: "Accounting and Banking",
+        contact: {
+            name: "John Smith",
+            role: "Senior Developer",
+            email: "john.smith@fake.com",
+            slack: "John Smith"
+        },
+        pairing: 80,
+        previousProteges: [{
+            name: "James Kehoe",
+            email: "james.kehoe@fake.com",
+            slack: "James Kehoe"
+        }, {
+            name: "Amanda Chau",
+            email: "amanda.chau@fake.com",
+            slack: "Amanda Chau"
+        }]
+    },
+    {
+        id: 2,
+        image:
+            "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=PastelGreen&graphicType=Diamond&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale",
+        teamName: "Area51",
+        stack: ["React", "C#"],
+        description: "Our team consists of 2 senior devs, a BA and a QA",
+        product: "All Products",
+        location: "Melbourne",
+        domain: "Bank Feeds",
+        contact: {
+            name: "Jane Smith",
+            role: "Delivery Manager",
+            email: "jane.smith@fake.com",
+            slack: "Jane Smith"
+        },
+        pairing: 100,
+        previousProteges: [{
+            name: "James Kehoe",
+            email: "james.kehoe@fake.com",
+            slack: "James Kehoe"
+        }, {
+            name: "Amanda Chau",
+            email: "amanda.chau@fake.com",
+            slack: "Amanda Chau"
+        }]
+    },
+    {
+        id: 3,
+        image:
+            "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight2&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=PastelGreen&graphicType=Diamond&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale",
+        teamName: "BiFrost",
+        stack: ["React", "C#"],
+        description: "Our team consists of 2 senior devs, a BA and a QA",
+        product: "New Essentials",
+        location: "Melbourne",
+        domain: "Some Domain",
+        contact: {
+            name: "Michael Smith",
+            role: "Senior Developer",
+            email: "michael.smith@fake.com",
+            slack: "Michael Smith"
+        },
+        pairing: 0,
+        previousProteges: []
+    }
+];
