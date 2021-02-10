0959aaf6e78700959f145227b49edb077f3ddcbe
Leah Filkin <leah.filkin@myob.com>
2021-02-10 15:49:31 +1300

    starting swipe functionality

--- rotation-tinder/src/SwipeyBoi.js
+++ rotation-tinder/src/SwipeyBoi.js
@@ -1,8 +1,21 @@
 import './App.css';
-import React from 'react';
-
+import React, {useState} from 'react';
+import TinderCard from 'react-tinder-card';
+import Cards, { Card } from 'react-swipe-card'
 
 const SwipeyBoi = (props) => {
+    const data = ['Alexandre', 'Thomas', 'Lucien']
+
+    const [lastDirection, setLastDirection] = useState()
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
 
     const allTeams = props.allTeams.map(team => {
         return (
@@ -12,9 +25,15 @@ const SwipeyBoi = (props) => {
 
 
     return (
-        <div>
-            {allTeams}
-        </div>
+        <Cards /*onEnd={action('end')}*/ className='master-root'>
+            {data.map(item =>
+                <Card
+                    /*                        onSwipeLeft={action('swipe left')}
+                                            onSwipeRight={action('swipe right')}*/>
+                    <h2>{item}</h2>
+                </Card>
+            )}
+        </Cards>
     );
 };
 
